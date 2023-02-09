e7dcd2c34db2052269b79e5671c463ac497785bc
lblake <treleven.lloyd@gmail.com>
2023-02-09 13:54:53 +0000

    working on code to test form submission

--- src/components/W12MForm.test.tsx
+++ src/components/W12MForm.test.tsx
@@ -1,12 +1,26 @@
-import { render } from '@testing-library/react';
-import W12MForm from './W12MForm';
+import { fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import W12MForm from "./W12MForm";
 
-test('renders form element', () => {
-	// we can hold onto the object returned from render()
-	// this object has a container property that we can destructure and inspect
-	const { container } = render(<W12MForm />);
-
-	// the container is just a normal DOM element, so we can look at normal properties like '.firstChild'
-	// for example, the firstChild of our container should be our form element
-	expect(container.firstChild).toHaveClass('w12MForm');
-});
+test("form render test", () => {
+  
+  const textBox_test_speciesName = {
+    id: "speciesName",
+    title: "Species Name",
+    min: "3",
+    max: "23",
+    pattern: "^[a-zA-Z ]+$",
+    displayError: false,
+    errMessage:
+      "Error - Please follow the rules : Enter only alphabets, at least 3 characters and maximum 17 characters!",
+  };
+  const textBox_test_planetName = {
+    id: "planetName",
+    title: "Planet Name",
+    min: "2",
+    max: "49",
+    pattern: "^[a-zA-Z0-9 ]+$",
+    displayError: false,
+    errMessage:
+      "Error - Please follow the rules : Enter only alphabets or numbers, atleast 2 characters and maximum 49 characters!",
+  };
