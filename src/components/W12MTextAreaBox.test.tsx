4fffc6ed4c543244f1ea22395af832fed06a8de8
lblake <treleven.lloyd@gmail.com>
2023-02-11 21:24:35 +0000

    added selectbox code and tests

---
+++ src/components/W12MTextAreaBox.test.tsx
@@ -0,0 +1,83 @@
+import { render, screen } from "@testing-library/react";
+import { fireEvent } from "@testing-library/react";
+
+import W12MTextAreaBox from "./W12MTextAreaBox";
+
+test("textareabox render test", () => {
+  const textAreaBox_test_reasonForSparing = {
+    id: "reasonForSparing",
+    title: "Reason for sparing",
+    min: 17,
+    max: 153,
+    displayError: false,
+    errMessage:
+      "Error - Please follow the rules : Enter atleast 17 characters and maximum 153 characters!",
+  };
+  const TextAreaBoxProps = {
+    textareaObject: textAreaBox_test_reasonForSparing,
+    onChange: jest.fn(),
+  };
+  render(<W12MTextAreaBox {...TextAreaBoxProps} />);
+  const textareaBox = screen.getByTestId("reasonForSparing");
+  expect(textareaBox).toBeInTheDocument();
+});
+
+test("textareabox value on Onchange test", () => {
+  const textAreaBox_test_reasonForSparing = {
+    id: "reasonForSparing",
+    title: "Reason for sparing",
+    min: 17,
+    max: 153,
+    displayError: false,
+    errMessage:
+      "Error - Please follow the rules : Enter atleast 17 characters and maximum 153 characters!",
+  };
+  const TextAreaBoxProps = {
+    textareaObject: textAreaBox_test_reasonForSparing,
+    onChange: jest.fn(),
+  };
+  render(<W12MTextAreaBox {...TextAreaBoxProps} />);
+  const textareaBox = screen.getByTestId("reasonForSparing");
+  fireEvent.change(textareaBox, {
+    target: { value: "genius is by hardwork" },
+  });
+  expect(textareaBox.textContent).toBe("genius is by hardwork");
+});
+
+test("textareabox errormessage render test", () => {
+  const textAreaBox_test_reasonForSparing = {
+    id: "reasonForSparing",
+    title: "Reason for sparing",
+    min: 17,
+    max: 153,
+    displayError: false,
+    errMessage:
+      "Error - Please follow the rules : Enter atleast 17 characters and maximum 153 characters!",
+  };
+  const TextAreaBoxProps = {
+    textareaObject: textAreaBox_test_reasonForSparing,
+    onChange: jest.fn(),
+  };
+  render(<W12MTextAreaBox {...TextAreaBoxProps} />);
+  const errMsg = screen.getByTestId("errormessage");
+  expect(errMsg).toBeInTheDocument();
+});
+
+test("textareabox errormessage visibility test", () => {
+  const textAreaBox_test_reasonForSparing = {
+    id: "reasonForSparing",
+    title: "Reason for sparing",
+    min: 17,
+    max: 153,
+    displayError: false,
+    errMessage:
+      "Error - Please follow the rules : Enter atleast 17 characters and maximum 153 characters!",
+  };
+  const TextAreaBoxProps = {
+    textareaObject: textAreaBox_test_reasonForSparing,
+    onChange: jest.fn(),
+  };
+  render(<W12MTextAreaBox {...TextAreaBoxProps} />);
+  const errMsg = screen.getByTestId("errormessage");
+  expect(errMsg).toHaveProperty("hidden", true);
+});
--- src/components/W12MTextAreaBox.tsx
+++ src/components/W12MTextAreaBox.tsx
@@ -1,1 +1,38 @@
-import React from 'react';
+import TextAreaStructure from "../data/textarea-struct";
+
+interface TextAreaBoxProps {
+  textareaObject: TextAreaStructure;
+  onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+const TextAreaBox: React.FC<TextAreaBoxProps> = ({
+  textareaObject,
+  onChange,
+}) => {
+  return (
+    <div>
+      <label className="form-control" htmlFor={textareaObject.id}>
+        {textareaObject.title}
+      </label>
+      <p>
+        <textarea
+          className="form-control"
+          data-testid={textareaObject.id}
+          minLength={textareaObject.min}
+          maxLength={textareaObject.max}
+          onChange={onChange}
+          required
+        ></textarea>
+      </p>
+      <p
+        data-testid="errormessage"
+        hidden={!textareaObject.displayError}
+        className="error"
+      >
+        {textareaObject.errMessage}
+      </p>
+    </div>
+  );
+};
+
+export default TextAreaBox;
